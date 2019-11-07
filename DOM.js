3c36ec8eedd7771e18fd3420142b21fd8ed6a278
Himanshu Pandey <himanshup895@gmail.com>
2019-11-07 23:13:25 +0530

    updated

---
+++ DOM.js
@@ -0,0 +1,6 @@
+/**
+ * window:
+ *  document (DOM)
+ *  BOM(i.e. navigator, screen, location, frames, history, XMLHttpRequest)
+ *  Javascript(i.e. Object, Array, Function)
+ */
---
+++ DetectBrowser.js
@@ -0,0 +1,23 @@
+/**
+ * console.log(navigator.userAgent)
+ * "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36"
+ * 
+ * indexOf(value) : return the position of the first occurence of specified value in a string, if no value to search return -1
+ */
+
+let OSName = 'Unknown OS';
+if (navigator.userAgent.indexOf("Win") != -1) OSName = "Windows";
+if (navigator.userAgent.indexOf("Mac") != -1) OSName = "Macintosh";
+if (navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
+if (navigator.userAgent.indexOf("Android") != -1) OSName = "Android";
+if (navigator.userAgent.indexOf("like Mac") != -1) OSName = "iOS";
+
+console.log('OS name: ', OSName);
+
+function handleClick(e) {
+    console.log('clicked');
+    return false
+}
+
+
+
